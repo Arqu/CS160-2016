d3243a049ed0c49cb62eb288b1b8ffb050c0613a
Asmir Avdicevic <asmir.avdicevic64@gmail.com>
2016-11-14 09:47:37 +0100

    Comments

--- 14-11/task-6.cpp
+++ 14-11/task-6.cpp
@@ -6,11 +6,18 @@ int n, x;
 double sum = 0, avg = 0, prod = 1;
 cout << "Input the number of numbers that you would like to calculate " << endl;
 cin >> n;
+// general practice is to go from 0 to <n since arrays are also 0 indexed thus you want to avoid calculating the indexes
+// in the future as it is error prone unless you really need it
+// another thing you should work on is your indentation and style consistency
+// you are already ahead of the majority but thats something you can improve
+// most notably indentation and spacing brackets - ){ => ) { and keep it consistent.
 for (int i = 1; i <= n; i++){
     cout << "Please input a number :" << endl;
     cin >> x;
 
     sum = sum + x;
+    // also you can optimise this part by moving the calculation below the 
+    // loop as this can be done just after we get the final sum
     avg = sum / n;
     prod = prod * x;
 
