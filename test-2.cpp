00f21e737cc6cf2c973bd10cd13c772c4615403f
Your Name <you@example.com>
2016-11-23 10:16:59 +0100

    This is the first commit

---
+++ test-2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Please enter an integer value: ";
+    int integer_value;
+    std::cin >> integer_value;
+    std::cout << "Your inputted integer value is: " << integer_value << std::endl;
+    return 0;
+}
+
+/*#include <iostream>
+ #include <string>
+ 
+ int main() {
+ std::string input;
+ std::cout << "Please enter a string: ";
+ std::getline(std::cin, input);
+ std::cout << "Yo, " << input << std::endl;
+ return 0;
+ } */
+
