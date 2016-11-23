668f9fde6dd466a2a8cae29ed00c85b0b1f2f952
nedim1511 <kurbegovicnedim@gmail.com>
2016-11-23 10:37:19 +0100

    Update and rename

--- 11-23/task-2.cpp
+++ 11-23/task-2.cpp
@@ -7,15 +7,3 @@ int main() {
     std::cout << "Your inputted integer value is: " << integer_value << std::endl;
     return 0;
 }
-
-/*#include <iostream>
- #include <string>
- 
- int main() {
- std::string input;
- std::cout << "Please enter a string: ";
- std::getline(std::cin, input);
- std::cout << "Yo, " << input << std::endl;
- return 0;
- } */
-
