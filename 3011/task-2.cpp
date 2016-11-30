7ad77bd97b64b479d7bda3f49017367fea691e1a
amnicaaaa <amna.subic@stu.ssst.edu.ba>
2016-11-30 18:28:45 +0100

    3011

---
+++ 3011/task-2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    int x;
+    cout << "enter x: ";
+    cin >> x;
+    cout << "it's square is: " << pow(x,2) << endl;
+    return (0);
+}
---
+++ 3011/task-3.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    cout << "Straight masterpieces" << endl;
+    cout << "Stylin', while in" << endl;
+    cout << "Livin' it up in the city" << endl;
+    return (0);
+}
